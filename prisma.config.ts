8808f993a2d8d85c1311c28db5c3af797dc01971
Marco Muller <marco.muller.mm@gmail.com>
2026-02-12 11:15:21 +0100

    correct prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,5 @@
-// prisma.config.ts
-import { defineConfig } from 'prisma/config';
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  datasource: {
-    //provider: 'postgresql', // or 'mysql', 'sqlite', etc.
-    url: process.env.DATABASE_URL, // Use your environment variable
-  },
+  schema: "src/db/schema.prisma",
 });
